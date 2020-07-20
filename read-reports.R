aa9d4a04174640a92dbf9008f05d590140766622
Travis Gerke <tgerke@hsph.harvard.edu>
2020-07-20 00:51:26 -0400

    style spacing

--- read-reports.R
+++ read-reports.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 
 txt <- pdftools::pdf_text(
-  here::here("reports", "pediatric_report_20200717.pdf")) %>%
+  here::here("reports", "pediatric_report_20200717.pdf")
+) %>%
   read_lines()
 
-counties <- txt %>% 
+counties <- txt %>%
   str_squish() %>%
   str_remove_all(",") %>%
   str_replace("Palm Beach", "Palm-Beach") %>%
@@ -15,18 +16,23 @@ counties <- txt %>%
   read_delim(delim = " ", skip = 27, n_max = 35, col_names = FALSE)
 
 counties <- bind_rows(
-  counties %>% 
+  counties %>%
     select(X1:X5) %>%
-    rename(County = X1,
-           Cases = X2,
-           Negative = X3,
-           Total = X4, 
-           'Percent Positive' = X5),
-  counties %>% 
+    rename(
+      County = X1,
+      Cases = X2,
+      Negative = X3,
+      Total = X4,
+      "Percent Positive" = X5
+    ),
+  counties %>%
     select(X6:X10) %>%
-    rename(County = X6,
-           Cases = X7,
-           Negative = X8,
-           Total = X9, 
-           'Percent Positive' = X10)
-)
+    rename(
+      County = X6,
+      Cases = X7,
+      Negative = X8,
+      Total = X9,
+      "Percent Positive" = X10
+    )) %>%
+  filter(!is.na(County))
+  

7a95602fc2d526d9d4479e8c56dc2cdbd15e5459
Travis Gerke <tgerke@hsph.harvard.edu>
2020-07-22 13:11:58 -0400

    produce per_capita tibble

--- results-per-capita.R
+++ results-per-capita.R
@@ -4,10 +4,22 @@ pop <- read_rds(here::here("data/FL-population-2020.rds"))
 counties <- read_rds(here::here("data/county-testing.rds"))
 
 pop <- pop %>%
-  filter(Age %in% c("0-4", "5-17")) %>%
+  filter(
+    Age %in% c("0-4", "5-17"),
+    County != "Florida"
+  ) %>%
   group_by(County) %>%
   tally(`Population estimate 2020`)
 
-per_capita <- map(counties, ~left_join(pop, .x, by = "County"))
+per_capita <- map(counties, ~ left_join(pop, .x, by = "County")) %>%
+  map(~ mutate(.x, "Tests per capita" = Total / n))
 
+dates <- names(per_capita) %>%
+  str_sub(start = -12, end = -5)
 
+per_capita <- map2(
+  per_capita,
+  dates,
+  ~ mutate(.x, date = lubridate::ymd(.y))
+) %>%
+  map_dfr(bind_rows)
